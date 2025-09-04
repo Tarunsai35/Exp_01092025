7447862ad2f0f23c20d5ab96c3091da90a22a8d2
Tarunsai35 <tarunsai5555@gmail.com>
2025-09-04 23:55:54 +0530

    day 3

--- src/test/java/zTestpackage/Pattens.java
+++ src/test/java/zTestpackage/Pattens.java
@@ -36,10 +36,46 @@ public class Pattens {
 		return;
 	}
 	
+	public static void day_3(int n) {
+		
+/*		
+		* * * * *
+		* * * *
+		* * *
+		* *
+		*
+		
+		     *
+		   * *
+     	 * * *
+	   * * * *
+	 * * * * *
+*/	
+//	for(int i = 1; i <= n; i++) {
+//		for(int j = n; j >= i; j--) {
+//			System.out.print("* ");
+//		}
+//		System.out.println();
+//	}
+	
+		for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+		}
+		return;
+	}
+	
+	
 	public static void main(String[] args) {
 		
 //		day_1(5);
-		day_2(5);
+//		day_2(5);
+		day_3(5);
 	}
 	
 }
